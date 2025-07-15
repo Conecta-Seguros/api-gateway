068e5776bdce1e14b126a37f9cbfcca8090e4d16
Maicol Montoya <maicolmontoya323@gmail.com>
2025-07-15 10:10:12 -0500

    feat: integrate dotenv for environment variable management

--- src/main/java/conectaseguros/co/api/gateway/ApiGatewayApplication.java
+++ src/main/java/conectaseguros/co/api/gateway/ApiGatewayApplication.java
@@ -1,14 +1,24 @@
 package conectaseguros.co.api.gateway;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.util.Objects;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./api-gateway")
+				.load();
+
+		System.setProperty("SPRING_OAUTH2_CLIENT_SECRET", Objects.requireNonNull(dotenv.get("SPRING_OAUTH2_CLIENT_SECRET")));
+
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
